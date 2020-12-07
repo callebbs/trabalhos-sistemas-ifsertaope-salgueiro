704b17ac3181f792d15040729dc891d7ed154275
Calleb Bezerra <calleb.bezerra@gmail.com>
2020-12-07 20:06:03 -0300

    atualização

---
+++ 2-periodo/linguagem de programação/aula 01 - 07-12-2020.py
@@ -0,0 +1,24 @@
+'''
+AULA 01
+
+== Tipo de Variáveis ==
+
+Python utiliza tipos de variáveis dinâmicas. Ou seja, ela aceita qualquer tipo de valor, sem necessitar de declarar seu tipo anteriormente.
+Ex:
+
+num = 10
+
+* Mostra qual o tipo da variável.
+type(num): retorna 'int' - integer: números inteiros
+
+num = 10.7
+
+type(num): retorna 'float' - float: números reais
+
+- Não é necessário declarar antes de atribuir valores.
+
+== Entrada de dados ==
+
+
+
+'''
