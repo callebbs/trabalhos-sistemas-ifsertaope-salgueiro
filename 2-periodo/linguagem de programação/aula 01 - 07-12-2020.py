f15d95dea39b5eb94f8a5e148c6f4ca9424d9310
Calleb Bezerra <calleb.bezerra@gmail.com>
2020-12-07 20:45:49 -0300

    aula 01 - 07-12-2020

--- 2-periodo/linguagem de programação/aula 01 - 07-12-2020.py
+++ 2-periodo/linguagem de programação/aula 01 - 07-12-2020.py
@@ -1,10 +1,15 @@
 '''
 AULA 01
 
-== Tipo de Variáveis ==
+========== Tipo de Variáveis ==========
 
 Python utiliza tipos de variáveis dinâmicas. Ou seja, ela aceita qualquer tipo de valor, sem necessitar de declarar seu tipo anteriormente.
-Ex:
+
+int = integer / números inteiros
+float = números reais / ponto flutuante
+string = palavras
+
+*** Ex:
 
 num = 10
 
@@ -17,8 +22,19 @@ type(num): retorna 'float' - float: números reais
 
 - Não é necessário declarar antes de atribuir valores.
 
-== Entrada de dados ==
+========== Entrada de dados ==========
 
+nome = input("Digite seu nome: ") - a função input() espera do usuário que insira os dados para armazenar na variável.
 
+========== Criando o primeiro programa ==========
+
+* Agora vamos criar um programa básico de média de notas:
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+
+media = (nota1 + nota2) / 2
+
+float é um tipo de dado de ponto flutuante / número real
 
 '''
