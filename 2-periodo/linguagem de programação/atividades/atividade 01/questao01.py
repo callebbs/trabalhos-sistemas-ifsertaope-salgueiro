8678338e771b7f453d14cc46ca94728bf6220db0
Calleb Bezerra <calleb.bezerra@gmail.com>
2020-12-08 09:46:01 -0300

    Update questao01.py

--- 2-periodo/linguagem de programação/atividades/atividade 01/questao01.py
+++ 2-periodo/linguagem de programação/atividades/atividade 01/questao01.py
@@ -2,3 +2,6 @@
 Escreva um programa que recebe uma temperatura em graus Celsius e a apresenta em graus Fahrenheit, de acordo com a fórmula abaixo:
 Fahrenheit = (1,8 x Celsius) + 32
 '''
+
+temperatura = float(input("Digite a temperatura em Graus Celcius: "))
+
