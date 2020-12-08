02db715fb464b70219a705e3b1cc4eaeae3f1210
Calleb Bezerra <calleb.bezerra@gmail.com>
2020-12-08 09:30:06 -0300

    atualizações

---
+++ 2-periodo/linguagem de programação/atividades/atividade 01/questao01.py
@@ -0,0 +1,4 @@
+'''
+Escreva um programa que recebe uma temperatura em graus Celsius e a apresenta em graus Fahrenheit, de acordo com a fórmula abaixo:
+Fahrenheit = (1,8 x Celsius) + 32
+'''
--- 2-periodo/linguagem de programação/atividades/exercicio01 - 07-12-2020.py
+++
@@ -1,5 +0,0 @@
-'''
-Escreva um programa que recebe uma temperatura em graus Celsius e a apresenta em graus Fahrenheit, de acordo com a fórmula abaixo:
-Fahrenheit = (1,8 x Celsius) + 32
-'''
-
