c3d039d8edc830448b3067365e0bf2819da31b12
Calleb Bezerra <calleb.bezerra@gmail.com>
2020-12-22 13:16:02 -0300

    resolução da questão 01

--- 2-periodo/linguagem de programação/atividades/atividade 03/atividade 03 - questão 01.py
+++ 2-periodo/linguagem de programação/atividades/atividade 03/atividade 03 - questão 01.py
@@ -3,3 +3,13 @@ Escreva um programa Python querecebe a idade de 10 pessoas, calcula e mostra a q
 (idade >= 18 anos)
 '''
 
+print("Bem vindo(a) ao programa de contador de idades.")
+cont = 0
+
+for i in range(10):
+    idade = float(input("Digite uma idade: "))
+    if idade >= 18:
+        cont = cont + 1
+
+print("Das idades digitadas, {} são de pessoas com maior idade.".format(cont))
+print("Fim do programa.")
