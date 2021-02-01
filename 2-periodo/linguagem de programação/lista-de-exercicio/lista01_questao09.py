f8112a2d67332425ebf46860a04ba5cb9bfaf0e8
Calleb Bezerra <calleb.bezerra@gmail.com>
2021-02-01 12:30:04 -0300

    resolução da questão 09

--- 2-periodo/linguagem de programação/lista-de-exercicio/lista01_questao09.py
+++ 2-periodo/linguagem de programação/lista-de-exercicio/lista01_questao09.py
@@ -3,3 +3,38 @@ Escreva um programa que lê duas notas de vários alunos e armazena estas notas 
 A entrada de dados deve terminar quando for lida  uma string vazia como nome. 
 Escreva uma função que permita retornar a média do aluno, dado  o seu nome como argumento. 
 '''
+
+def media(nome):
+    nota1 = alunos[nome] [0]
+    nota2 = alunos[nome] [1]
+    media = (nota1 + nota2) / 2
+    return media
+
+#--------------------------------------------------------------------------------------------------
+
+alunos = {}
+nome = "teste"
+
+while (nome != ""):
+    nome = input("Digite o nome do aluno(a): ")
+    if nome == "":
+        print("Fim do Cadastro")
+        print("=" * 30)
+        break
+    else:
+        nota1 = float(input("Digite a primeira nota: "))
+        nota2 = float(input("Digite a segunda nota: "))
+        alunos[nome] = [nota1, nota2]
+
+print("Lista de Alunos Cadastrados:")
+print("-" * 30)
+for i in alunos:
+    print(i)
+print("-" * 30)
+
+resposta = "sim"
+while (resposta != "não"):
+    nomeMedia = input("Digite o nome para visualizar a média: ")
+    print("A média do aluno(a) {} é {}".format(nomeMedia, media(nomeMedia)))
+
+    resposta = input("Deseja continuar? Digite SIM ou NÃO: ")
