6649c4dd20fee8f83e51205bfc5a1a44f5dd5549
Calleb Bezerra <calleb.bezerra@gmail.com>
2021-02-04 17:03:28 -0300

    metade da resolução da questão 10

--- 2-periodo/linguagem de programação/lista-de-exercicio/lista01_questao11.py
+++ 2-periodo/linguagem de programação/lista-de-exercicio/lista01_questao11.py
@@ -3,7 +3,7 @@ Escreva um programa para armazenar uma agenda de telefones em um dicionário.
 Cada pessoa  pode ter um ou mais telefones e a chave do dicionário é o nome da pessoa. 
 
 Seu programa deve  ter as seguintes opções (todas realizadas através de funções próprias criadas por você): 
-a. Incluir Novo Nome – permite acrescentar um novo nome na agenda, com um ou mais  telefones. 
+a. Incluir Novo Nome – permite acrescentar um novo nome na agenda, com um ou mais telefones. 
 b. Incluir Telefone – acrescenta um telefone em um nome já existente na agenda. 
 Caso o  nome não exista na agenda, você deve perguntar se a pessoa deseja incluí-lo. 
 c. Excluir Telefone – exclui um telefone de uma pessoa que já está na agenda. 
