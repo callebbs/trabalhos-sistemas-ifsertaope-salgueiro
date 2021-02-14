875b200c0f4f2229618b4d38f98d9eaac6f6de9d
Calleb Bezerra <calleb.bezerra@gmail.com>
2021-02-14 15:42:38 -0300

    resolução questão 12

--- 2-periodo/linguagem de programação/lista-de-exercicio/lista01_questao12.py
+++ 2-periodo/linguagem de programação/lista-de-exercicio/lista01_questao12.py
@@ -1,3 +1,21 @@
 '''
 Implemente uma função que, dado um número n maior que 2, devolve uma String com todos os números primos menores ou iguais a n, separados por espaços em branco. 
 '''
+
+def NumerosPrimos(numero):
+    primos = ""
+    for i in range(2, numero+1):
+        contador = 0
+        for j in range(2, i+1):
+            if i % j == 0:
+               contador += 1
+        if contador <= 1:
+            primos = primos + " " + str(i)
+    return primos
+
+print("Listagem de Números Primos: ")
+numero = int(input("Digite o número: "))
+if numero >= 2:
+    print(f"Os números primos são:{NumerosPrimos(numero)}")
+else:
+    print("Número inválido. Digite um número acima de 2.")
