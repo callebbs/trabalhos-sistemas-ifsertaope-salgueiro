9a170bcea4664d2ca2b8db370012d5a7002939f7
Calleb Bezerra <calleb.bezerra@gmail.com>
2021-02-14 17:26:24 -0300

    Update lista01_questao19.py

--- 2-periodo/linguagem de programação/lista-de-exercicio/lista01_questao19.py
+++ 2-periodo/linguagem de programação/lista-de-exercicio/lista01_questao19.py
@@ -1,5 +1,5 @@
 '''
-Implemente uma função chamada eliminarRepetidos([ Int ]) 🡪 [ Int ], que recebe uma lista de  inteiros e devolve a mesma lista, mas sem elementos repetidos, mantendo apenas a primeira  ocorrência de cada elemento. 
+Implemente uma função chamada eliminarRepetidos([ Int ]) 🡪 [ Int ], que recebe uma lista de  inteiros e devolve a mesma lista, mas sem elementos repetidos, mantendo apenas a primeira ocorrência de cada elemento. 
 
 Observações: 
 a. Não utilize strings para a solução do problema. 
