1f4d65407463b978edb532e46cc1798f8ba648ae
Calleb Bezerra <calleb.bezerra@gmail.com>
2021-02-15 10:40:46 -0300

    questões não feitas

--- 2-periodo/linguagem de programação/lista-de-exercicio/lista01_questao16.py
+++
@@ -1,6 +0,0 @@
-'''
-Defina uma função inverterOrdem(String) 🡪 String, 
-que recebe uma string contendo uma frase  de palavras separadas pelo caractere “ ” (branco) e retorna a frase com a ordem das palavras  invertida. 
-
-Obs.: Todas as funções auxiliares para a resolução do problema devem ser criadas por  você. 
-'''
