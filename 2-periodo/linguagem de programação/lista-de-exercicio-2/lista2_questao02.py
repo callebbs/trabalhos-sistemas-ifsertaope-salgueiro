72de283c19ac09faac44360090c1951e331c91ee
Calleb Bezerra <calleb.bezerra@gmail.com>
2021-03-17 12:43:51 -0300

    Update lista2_questao02.py

---
+++ 2-periodo/linguagem de programação/lista-de-exercicio-2/lista2_questao02.py
@@ -0,0 +1,3 @@
+'''
+Faça um programa Python que, através de uma API JSON, obtenha a cotação do dólar e do euro. O programa deve receber um valor em reais do teclado e convertê-lo para dólares e euros. Como saída, a aplicação deverá apresentar a cotação do dólar e do euro e a conversão dos reais fornecidos como entrada.
+'''
