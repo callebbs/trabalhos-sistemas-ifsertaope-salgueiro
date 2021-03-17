c9272eba339b40ac4e19a2a23b5294d0d9bec375
Calleb Bezerra <calleb.bezerra@gmail.com>
2021-03-17 12:44:28 -0300

    Update lista2_questao04.py

---
+++ 2-periodo/linguagem de programação/lista-de-exercicio-2/lista2_questao04.py
@@ -0,0 +1,3 @@
+'''
+Utilizando uma API JSON e o módulo Matplotlib, desenvolva uma aplicação que recupera os dados da COVID-19 no Brasil e apresenta sua evolução em gráficos. Apresente um gráfico geral do Brasil, juntamente com os dados dos contaminados e mortos e também permita que o usuário possa escolher algum estado brasileiro que ele deseja ver o gráfico e os dados.
+'''
