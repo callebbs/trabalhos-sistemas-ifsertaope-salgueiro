d19fb9f53902599f4e345837731e876f740ef683
Calleb Bezerra <calleb.bezerra@gmail.com>
2021-03-17 12:44:26 -0300

    Update lista2_questao03.py

---
+++ 2-periodo/linguagem de programação/lista-de-exercicio-2/lista2_questao03.py
@@ -0,0 +1,3 @@
+'''
+Desenvolva uma aplicação Python para fazer buscar de temas específicos no Twitter. O programa deve solicitar do usuário uma palavra-chave/hashtag e recuperar 1000 mensagens do Twitter (no formato JSON) que possuam a palavra-chave/hashtag fornecida pelo teclado.
+'''
