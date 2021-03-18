34ad8f1fd0f85ea36ba18301060b55abbe8c6fa5
Calleb Bezerra <calleb.bezerra@gmail.com>
2021-03-18 14:05:08 -0300

    resolução questão 02

--- 2-periodo/linguagem de programação/lista-de-exercicio-2/lista2_questao02.py
+++ 2-periodo/linguagem de programação/lista-de-exercicio-2/lista2_questao02.py
@@ -4,3 +4,39 @@ O programa deve receber um valor em reais do teclado e convertê-lo para dólare
 Como saída, a aplicação deverá apresentar a cotação do dólar e do euro e a conversão 
 dos reais fornecidos como entrada.
 '''
+
+import json
+import requests
+
+url = "https://economia.awesomeapi.com.br/json/all"
+
+resposta = requests.get(url)
+resposta.raise_for_status
+
+dados = json.loads(resposta.text)
+
+dolar_comercial = dados["USD"]
+dolar_turismo = dados["USDT"]
+euro = dados["EUR"]
+
+valor = int(input("Digite o valor em REAIS para converter: "))
+
+print("-" * 45)
+print(f"Data e Horário: {dolar_comercial['create_date']}")
+print("-" * 45)
+print(f"Cotação do {dolar_comercial['name']}: {dolar_comercial['high']}")
+print(f"Cotação do {dolar_turismo['name']}: {dolar_turismo['high']}")
+print("-" * 45)
+print(f"Data e Horário: {euro['create_date']}")
+print(f"Cotação do {euro['name']}: {euro['high']}")
+print("-" * 45)
+print(f"Conversão:")
+
+dolar_atual = float(dolar_comercial['high'])
+euro_atual = float(euro['high'])
+
+conversao_dolar = valor / int(dolar_atual)
+conversao_euro = valor / int(euro_atual)
+print(f"R${valor} para USD: {conversao_dolar:.2f} dólares.")
+print(f"R${valor} para EUR: {conversao_euro:.2f} euros.")
+print("-" * 45)
