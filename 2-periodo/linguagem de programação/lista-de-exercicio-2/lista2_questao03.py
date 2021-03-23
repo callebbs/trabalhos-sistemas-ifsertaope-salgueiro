d37e84c96aa6480e6c7621e04f1addd464c7a7be
Calleb Bezerra <calleb.bezerra@gmail.com>
2021-03-23 17:07:25 -0300

    Update lista2_questao03.py

--- 2-periodo/linguagem de programação/lista-de-exercicio-2/lista2_questao03.py
+++ 2-periodo/linguagem de programação/lista-de-exercicio-2/lista2_questao03.py
@@ -1,5 +1,43 @@
 '''
-Desenvolva uma aplicação Python para fazer buscar de temas específicos no Twitter. 
-O programa deve solicitar do usuário uma palavra-chave/hashtag e recuperar 1000 mensagens do Twitter 
-(no formato JSON) que possuam a palavra-chave/hashtag fornecida pelo teclado.
+TRADUTOR: Implemente uma aplicação que recebe, através do teclado, 
+um texto em português e o traduza para o inglês ou espanhol com a API JSON do Google Tradutor, 
+conforme escolha do usuário. Por fim, apresente o texto traduzido na tela.
 '''
+
+import requests
+import json
+import os
+import urllib.parse
+
+url = "https://google-translate1.p.rapidapi.com/language/translate/v2"
+
+texto = str(input('Digite o texto para tradução: '))
+texto_convertido = urllib.parse.quote(texto)
+print(texto_convertido)
+
+idioma_traducao = str(input("Qual idioma quer traduzir o texto?\n** Digite en para Inglês **\n** Digite es para Espanhol **\nDigite sua escolha: "))
+
+payload = f"q={texto_convertido}&source=pt&target={idioma_traducao}"
+
+headers = {
+    'content-type': "application/x-www-form-urlencoded",
+    'accept-encoding': "application/gzip",
+    'x-rapidapi-key': "1cd986695dmsh8e7366a93c80b37p1c06b2jsnb49e41a57c3b",
+    'x-rapidapi-host': "google-translate1.p.rapidapi.com"
+    }
+
+traducao = requests.request("POST", url, data=payload, headers=headers)
+
+os.system('cls')
+dados = json.loads(traducao.text)
+print("-" * 50)
+print(f"Texto original: {texto}\n")
+
+if idioma_traducao == "en":
+    print(f"Tradução do Português do Brasil para Inglês:")
+elif idioma_traducao == "es":
+    print(f"Tradução do Português do Brasil para Espanhol:")
+
+print(dados["data"]["translations"][0]['translatedText'])
+print("-" * 50)
+print(payload)
