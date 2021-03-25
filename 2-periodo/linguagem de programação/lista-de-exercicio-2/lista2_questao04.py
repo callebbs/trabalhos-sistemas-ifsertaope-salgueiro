e426dfda08332d71b40c889b9ba4f71d92294d46
Calleb Bezerra <calleb.bezerra@gmail.com>
2021-03-25 00:44:10 -0300

    Update lista2_questao04.py

--- 2-periodo/linguagem de programação/lista-de-exercicio-2/lista2_questao04.py
+++ 2-periodo/linguagem de programação/lista-de-exercicio-2/lista2_questao04.py
@@ -5,11 +5,17 @@ juntamente com os dados dos contaminados e mortos e também permita que
 o usuário possa escolher algum estado brasileiro que ele deseja ver o gráfico e os dados.
 '''
 
-#obtendo dados da API
+import os
 import matplotlib.pyplot as grafico
 import json
 import requests
 
+#função pra limpar tela
+def clear():
+    import os
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+#obtendo dados da API
 url = "https://api.apify.com/v2/datasets/3S2T1ZBxB9zhRJTBB/items?format=json&clean=1"
 
 resposta = requests.get(url)
@@ -58,7 +64,7 @@ for i in range(0, len(lista_estados)):
 
 print(f" ** Dados do Estado de {estado} **")
 
-#variáveis e listas para armazenar dados
+#variáveis e listas para armazenar dados dos Estados
 infectados_estados = []
 dias_infectados_estados = []
 mortes_estados = []
@@ -68,7 +74,7 @@ quantidade_dias_mortes_estados = 0
 quantidade_infectados_estados = 0
 quantidade_mortes_estados = 0
 
-#dados infectados por Estado
+#obtendo dados infectados por Estado
 for i in range(0, 508):
     for j in range(0, len(dados[i]['infectedByRegion'])):
         if dados[i]['infectedByRegion'][j]['state'] == sigla_estado:
@@ -104,3 +110,54 @@ grafico.title(f'Dados de Mortes em {estado}')
 grafico.xlabel("Dias corridos da Pandemia")
 grafico.ylabel("Quantidade de mortes")
 grafico.show()
+
+#variáveis e listas para armazenar dados do Brasil
+infectados_brasil = []
+dias_infectados_brasil = []
+mortes_brasil = []
+dias_mortes_brasil = []
+quantidade_infectados_brasil = 0
+quantidade_dias_infectados_brasil = 0
+quantidade_mortes_brasil = 0
+quantidade_dias_mortes_brasil = 0
+
+#obtendo dados dos infectados no Brasil
+for i in range(0, 509):
+    quantidade_infectados_brasil = dados[i]['infected']
+    infectados_brasil.append(quantidade_infectados_brasil)
+    quantidade_dias_infectados_brasil = quantidade_dias_infectados_brasil + 1
+    dias_infectados_brasil.append(quantidade_dias_infectados_brasil)
+
+#gerando gráficos de infectados no Brasil
+grafico.plot(dias_infectados_brasil, infectados_brasil)
+grafico.title(f'Dados de Infectados no Brasil')
+grafico.xlabel("Dias corridos da Pandemia")
+grafico.ylabel("Quantidade de infectados em milhões")
+grafico.show()
+
+#obtendo dados de mortes no Brasil
+for i in range(0, 509):
+    quantidade_mortes_brasil = dados[i]['deceased']
+    mortes_brasil.append(quantidade_mortes_brasil)
+    quantidade_dias_mortes_brasil = quantidade_dias_mortes_brasil + 1
+    dias_mortes_brasil.append(quantidade_dias_mortes_brasil)
+
+#gerando gráficos de mortes no Brasil
+grafico.plot(dias_mortes_brasil, mortes_brasil)
+grafico.title(f'Dados de Mortes no Brasil')
+grafico.xlabel("Dias corridos da Pandemia")
+grafico.ylabel("Quantidade de mortes")
+grafico.show()
+
+#resumo dos dados
+
+clear()
+print("Resumo dos dados: ")
+print(f'-' * 50)
+print(f"Total de Infectados no Brasil: {infectados_brasil[-1]}")
+print(f"Total de Mortes no Brasil: {mortes_brasil[-1]}")
+print(f"Total de dias de pandemia: {quantidade_dias_infectados_brasil}")
+print(f'-' * 50)
+print(f'Total de Infectados em {estado}: {infectados_estados[-1]}')
+print(f'Total de Mortes em {estado}: {mortes_estados[-1]}')
+print(f'-' * 50)
