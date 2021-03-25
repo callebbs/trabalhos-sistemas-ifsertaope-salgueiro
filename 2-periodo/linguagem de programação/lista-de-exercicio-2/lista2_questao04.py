26d7d6bc8ca9fac4cedab6108724262d33f464e6
Calleb Bezerra <calleb.bezerra@gmail.com>
2021-03-24 23:58:17 -0300

    Update lista2_questao04.py

--- 2-periodo/linguagem de programação/lista-de-exercicio-2/lista2_questao04.py
+++ 2-periodo/linguagem de programação/lista-de-exercicio-2/lista2_questao04.py
@@ -5,7 +5,7 @@ juntamente com os dados dos contaminados e mortos e também permita que
 o usuário possa escolher algum estado brasileiro que ele deseja ver o gráfico e os dados.
 '''
 
-#pegando dados da API
+#obtendo dados da API
 import matplotlib.pyplot as grafico
 import json
 import requests
@@ -17,34 +17,71 @@ resposta.raise_for_status
 dados = json.loads(resposta.text)
 
 #interação com usuário
+sigla_estado = " "
 print("Programa de geração de gráficos da COVID-19 no Brasil.")
-estado = input("Digite a sigla do estado para ver os dados do COVID-19: ")
-print(f"Apresentando dados do estado {estado}")
+lista_estados = {
+    "Acre": "AC",
+    "Alagoas": "AL",
+    "Amapá": "AP",
+    "Amazonas": 'AM',
+    "Bahia": "BA",
+    "Ceará": "CE",
+    "Distrito Federal": "DF",
+    "Espírito Santo": "ES",
+    "Goiás": "GO",
+    "Maranhão": "MA",
+    "Mato Grosso": "MT",
+    "Mato Grosso do Sul": "MS",
+    "Minas Gerais:": "MG",
+    "Pará": "PA",
+    "Paraíba": "PB",
+    "Pernambuco": "PE",
+    "Piauí": "PI",
+    "Rio de Janeiro": "RJ",
+    "Rio Grande do Norte": "RN",
+    "Rio Grande do Sul": "RS",
+    "Rondônia": "RO",
+    "Roraima": "RR",
+    "Santa Catarina": "SC",
+    "São Paulo": "SP",
+    "Sergipe": "SE",
+    "Tocantins": "TO"
+}
+estado = input("Digite o nome estado para ver os dados do COVID-19: ")
 
+for i in range(0, len(lista_estados)):
+    if estado in lista_estados:
+        sigla_estado = lista_estados[estado]
+    else:
+        print("Estado inválido.")
+        break
 
-infectados = []
-mortes = []
-dias_infectados = []
-dias_mortes = []
-quantidade_dias_infectados = 0
-quantidade_dias_mortes = 0
-quantidade_infectados = 0
-quantidade_mortes = 0
+print(f" ** Dados do Estado de {estado} **")
+
+#variáveis e listas para armazenar dados
+infectados_estados = []
+dias_infectados_estados = []
+mortes_estados = []
+dias_mortes_estados = []
+quantidade_dias_infectados_estados = 0
+quantidade_dias_mortes_estados = 0
+quantidade_infectados_estados = 0
+quantidade_mortes_estados = 0
 
 #dados infectados por Estado
 for i in range(0, 508):
     for j in range(0, len(dados[i]['infectedByRegion'])):
-        if dados[i]['infectedByRegion'][j]['state'] == estado:
-            quantidade_infectados = dados[i]['infectedByRegion'][j]['count']
-            infectados.append(quantidade_infectados)
-            quantidade_dias_infectados = quantidade_dias_infectados + 1
-            dias_infectados.append(quantidade_dias_infectados)
+        if dados[i]['infectedByRegion'][j]['state'] == sigla_estado:
+            quantidade_infectados_estados = dados[i]['infectedByRegion'][j]['count']
+            infectados_estados.append(quantidade_infectados_estados)
+            quantidade_dias_infectados_estados = quantidade_dias_infectados_estados + 1
+            dias_infectados_estados.append(quantidade_dias_infectados_estados)
         else:
             continue
 
 #gerando gráficos de infectados por Estado
 print("Gerando gráficos...")
-grafico.plot(dias_infectados, infectados)
+grafico.plot(dias_infectados_estados, infectados_estados)
 grafico.title('Dados de Infectados em Pernambuco')
 grafico.xlabel("Dias corridos da Pandemia")
 grafico.ylabel("Quantidade de infectados")
@@ -53,17 +90,17 @@ grafico.show()
 #dados mortes por Estado
 for i in range(1, 509):
     for j in range(0, len(dados[i]['deceasedByRegion'])):
-        if dados[i]['deceasedByRegion'][j]['state'] == estado:
+        if dados[i]['deceasedByRegion'][j]['state'] == sigla_estado:
             quantidade_mortes = dados[i]['deceasedByRegion'][j]['count']
-            mortes.append(quantidade_mortes)
-            quantidade_dias_mortes = quantidade_dias_mortes + 1
-            dias_mortes.append(quantidade_dias_mortes)
+            mortes_estados.append(quantidade_mortes)
+            quantidade_dias_mortes_estados = quantidade_dias_mortes_estados + 1
+            dias_mortes_estados.append(quantidade_dias_mortes_estados)
         else:
             continue
 
 #gerando gráficos de mortes por Estado
-grafico.plot(dias_mortes, mortes)
-grafico.title(f'Dados de Mortes em Pernambuco')
+grafico.plot(dias_mortes_estados, mortes_estados)
+grafico.title(f'Dados de Mortes em {estado}')
 grafico.xlabel("Dias corridos da Pandemia")
 grafico.ylabel("Quantidade de mortes")
 grafico.show()
