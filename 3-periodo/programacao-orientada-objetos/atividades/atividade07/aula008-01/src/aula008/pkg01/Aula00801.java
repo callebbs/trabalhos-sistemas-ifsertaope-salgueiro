45b27a84eadb664be726c43f0bb5a0a67a404a03
Calleb Bezerra <calleb.bezerra@gmail.com>
2021-06-08 22:10:16 -0300

    aula 008 - POO

---
+++ 3-periodo/programacao-orientada-objetos/atividades/atividade07/aula008-01/src/aula008/pkg01/Aula00801.java
@@ -0,0 +1,14 @@
+package aula008.pkg01;
+
+// Aula sobre Herança
+
+public class Aula00801 {
+    public static void main(String[] args) {
+        Ponto2D p2d = new Ponto2D(6, 10);
+        Ponto3D p3d = new Ponto3D(10, 7, 18);
+        
+        System.out.println(p2d);
+        System.out.println(p3d);
+    }
+    
+}
