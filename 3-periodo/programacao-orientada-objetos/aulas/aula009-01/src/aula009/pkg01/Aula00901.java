5d0c5127345eb14f46890e3f40ee9a5eeccfdf89
Calleb Bezerra <calleb.bezerra@gmail.com>
2021-06-13 16:32:32 -0300

    aula 009

--- 3-periodo/programacao-orientada-objetos/aulas/aula009-01/src/aula009/pkg01/Aula00901.java
+++ 3-periodo/programacao-orientada-objetos/aulas/aula009-01/src/aula009/pkg01/Aula00901.java
@@ -5,13 +5,20 @@ public class Aula00901 {
 
     public static void main(String[] args) {
         
+        /*
         Aluno aluno1 = new Aluno();
         Professor professor1 = new Professor();
         
-        aluno1.setNome("Maria");
-        aluno1.getNome();
+        aluno1.setCurso("Sistemas para Internet");
         
+        */
         
+        Pessoa aluno = new Aluno();
+        
+        //aluno.getNome()
+        
+        Pessoa professor = new Professor();
+
     }
     
 }
