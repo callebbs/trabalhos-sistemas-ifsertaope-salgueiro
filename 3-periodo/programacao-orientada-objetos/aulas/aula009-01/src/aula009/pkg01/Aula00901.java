2bace554f1a501e7ec71a05ae33be9fe656faa2f
Calleb Bezerra <calleb.bezerra@gmail.com>
2021-06-13 13:53:15 -0300

    organização de arquivos e aula 008

---
+++ 3-periodo/programacao-orientada-objetos/aulas/aula009-01/src/aula009/pkg01/Aula00901.java
@@ -0,0 +1,17 @@
+package aula009.pkg01;
+
+public class Aula00901 {
+
+
+    public static void main(String[] args) {
+        
+        Aluno aluno1 = new Aluno();
+        Professor professor1 = new Professor();
+        
+        aluno1.setNome("Maria");
+        aluno1.getNome();
+        
+        
+    }
+    
+}
