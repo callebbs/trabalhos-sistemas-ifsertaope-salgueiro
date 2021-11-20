7acef78ba32cb5a913a371286efe094a9251035d
Calleb Bezerra <53497665+callebbs@users.noreply.github.com>
2021-11-20 20:50:16 -0300

    Atividade de Lista Encadeada

--- 4-periodo/estrutura-de-dados/atividades/atividade03-ed/src/atividade03/ed/Atividade03Ed.java
+++ 4-periodo/estrutura-de-dados/atividades/atividade03-ed/src/atividade03/ed/Atividade03Ed.java
@@ -3,6 +3,13 @@ package atividade03.ed;
 public class Atividade03Ed {
 
     public static void main(String[] args) {
+       ListaEncadeada lista = new ListaEncadeada();
+       lista.adicionaNoFinal("Estruturas de Dados é top");
+       System.out.println(lista.toString());
+       lista.adicionaNoFinal("Assista Star Wars");
+       System.out.println(lista.toString());
+       lista.adicionaNoFinal("Pela Ordem de lançento!");
+       System.out.println(lista.toString());
        
     }
     
