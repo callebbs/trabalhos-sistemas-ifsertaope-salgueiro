08f3026208584c9652ac3642fa9e3adba1526a05
Calleb Bezerra <53497665+callebbs@users.noreply.github.com>
2021-12-05 14:19:53 -0300

    Listas Encadeadas - Estrutura de Dados

--- 4-periodo/estrutura-de-dados/atividades/Aula12-ED/src/aula12/ed/Aula12ED.java
+++ 4-periodo/estrutura-de-dados/atividades/Aula12-ED/src/aula12/ed/Aula12ED.java
@@ -4,17 +4,29 @@ public class Aula12ED {
 
     public static void main(String[] args) {
        ListaEncadeada lista = new ListaEncadeada();
-        System.out.println("Aidionanco no início...");
-       lista.adicionaInicio("Star Wars é top");
-       System.out.println(lista.toString());
-       lista.adicionaInicio("Matrix é top");
-       System.out.println(lista.toString());
-       lista.adicionaInicio("Spice must flow!");
-       System.out.println(lista.toString());
-       
-       System.out.println("Adicionando no final...");
-       lista.adicionaNoFinal("Spider-man vai ser bom!");
-       System.out.println(lista.toString());
+//       System.out.println("Aidionanco no início...");
+//       lista.adicionaNoInicio("Star Wars é top");
+//       System.out.println(lista.toString());
+//       lista.adicionaNoInicio("Matrix é top");
+//       System.out.println(lista.toString());
+//       lista.adicionaNoInicio("Spice must flow!");
+//       System.out.println(lista.toString());
+//       
+//       System.out.println("Adicionando no final...");
+//       lista.adicionaNoFinal("Spider-man vai ser bom!");
+//       System.out.println(lista.toString());
+
+        lista.adicionaNoInicio("12");
+        lista.adicionaNoFinal("98");
+        lista.adicionaNoInicio("35");
+        lista.adicionaNoFinal("40");
+        System.out.println(lista.toString());
+        
+        lista.adicionaNoFinal("20");
+        System.out.println(lista.toString());
+        
+        lista.adiciona(2, 100);
+        System.out.println(lista.toString());
     }
     
 }
--- 4-periodo/estrutura-de-dados/atividades/Aula12-ED/src/aula12/ed/ListaEncadeada.java
+++ 4-periodo/estrutura-de-dados/atividades/Aula12-ED/src/aula12/ed/ListaEncadeada.java
@@ -5,7 +5,7 @@ public class ListaEncadeada {
     private No fim;
     private int totalDeElementos;
     
-    public void adicionaInicio(Object elemento) {
+    public void adicionaNoInicio(Object elemento) {
         No novo = new No(elemento, this.inicio);
         this.inicio = novo;
         
@@ -17,7 +17,7 @@ public class ListaEncadeada {
     
     public void adicionaNoFinal(Object elemento) {
         if(this.totalDeElementos == 0) {
-            this.adicionaInicio(elemento);
+            this.adicionaNoInicio(elemento);
         } else {
             No novo = new No(elemento);
             this.fim.setProximo(novo);
@@ -41,6 +41,34 @@ public class ListaEncadeada {
         builder.append("]");
         return builder.toString();
     }
+    //Verifica se a posição é válida
+    private boolean posicaoValida(int posicao) {
+        return posicao >= 0 && posicao < this.totalDeElementos;
+    }
     
+    //Retorna a posição atual
+    private No pegaNo(int posicao) {
+        if(!this.posicaoValida(posicao)) {
+            throw new IllegalArgumentException("Posição inválida");
+        }
+        No atual = this.inicio;
+        for(int i = 0; i < posicao - 1; i++) {
+            atual = atual.getProximo();
+        }
+        return atual;    
+    }
+    
+    public void adiciona(int posicao, Object elemento) {
+        if(posicao == 0) {
+            this.adicionaNoInicio(elemento);
+        } else if(posicao == this.totalDeElementos) {
+            this.adicionaNoFinal(elemento);
+        } else {
+            No anterior = this.pegaNo(posicao);
+            No novo = new No(elemento, anterior.getProximo());
+            anterior.setProximo(novo);
+            this.totalDeElementos++;
+        } 
+    }
     
 }
